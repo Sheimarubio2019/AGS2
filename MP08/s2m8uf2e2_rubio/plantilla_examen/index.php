5d1789f3d0f1b42c8b47f32fc09bad4d3cf88464
Sheimarubio2019 <56447135+Sheimarubio2019@users.noreply.github.com>
2020-04-08 17:45:20 +0200

    ordre i nateja

---
+++ MP08/s2m8uf2e2_rubio/plantilla_examen/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CarsandFilms</title>
+  <link rel="stylesheet" href="css/960.css">
+  <link rel="stylesheet" href="css/estils.css">
+
+</head>
+<body>
+  <div class="container_12 contenidor">
+    <div id="logoo" class="grid_6 push_4">
+      <h1>cars</h1>
+      <h2>and</h2>
+      <h1>films</h1>
+    </div>
+
+   <div id="ani" class="grid_8 push_2">
+   <img src="css/img/ani/back.png">
+   </div>
+
+    <div class="clear"></div>
+    <div id="body">
+    <div id="primera" class="grid_3">
+    <img src="css/img/small/small_pulp.jpg">
+    <h2>Pulp Fiction</h2>
+    </div>
+
+    <div id="segunda" class="grid_3 push_1">
+    <img src="css/img/small/small_taxi.jpg">
+    <h2>Small Taxi</h2>
+    </div>
+
+    <div id="tercera" class="grid_3 push_2">
+    <img src="css/img/small/small_bond.jpg">
+    <h2>SGoldfinger</h2>
+    </div>
+
+
+
+
+    <div id="pie">
+    <div class="grid_4 ">
+
+
+    </div>
+
+
+        </div>
+
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+  </body>
+  </html>
